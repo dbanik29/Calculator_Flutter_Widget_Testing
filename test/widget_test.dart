6e998a8c1dd321b6fd3b5007f3aba6b24103b3aa
dbanik29 <dolonbanik29@gmail.com>
2021-08-25 14:29:41 +0600

    Widget test first

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:calculator/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Check Calculator Texts', (WidgetTester tester) async{
+      await tester.pumpWidget(Calculator());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      await tester.pump();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Assert
+      expect(find.text('1'), findsWidgets);
+      expect(find.text('2'), findsWidgets);
+      expect(find.text('3'), findsWidgets);
+      expect(find.text('4'), findsWidgets);
+      expect(find.text('5'), findsWidgets);
+      expect(find.text('6'), findsWidgets);
+      expect(find.text('7'), findsWidgets);
+      expect(find.text('8'), findsWidgets);
+      expect(find.text('9'), findsWidgets);
+      expect(find.text('0'), findsWidgets);
+      expect(find.text('+'), findsWidgets);
+      expect(find.text('-'), findsWidgets);
+      expect(find.text('⌫'), findsWidgets);
+      expect(find.text('C'), findsWidgets);
+      expect(find.text('='), findsWidgets);
+      expect(find.text('100'), findsNothing);
+      expect(find.text('*'), findsNothing);
+      expect(find.text('|'), findsNothing);
+    });
+  }
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+
